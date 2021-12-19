b23f724e555b3e70441b7f428d4e33915d759415
Kaja Wieczorek <hibikyuu@gmail.com>
2021-12-19 19:24:58 +0100

    Add listening on all addresses

--- PakaUsers/Program.cs
+++ PakaUsers/Program.cs
@@ -14,6 +14,10 @@ namespace PakaUsers
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseUrls("http://*:5000", "https://*:5001");
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
